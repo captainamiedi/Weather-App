3487ce007b1e45366eabd86924930da2e2ad96ff
captainamiedi <captainamiedi1@gmail.com>
2019-09-16 11:43:19 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class App extends React.Component {
+    render () {
+        return (
+            <div>hello app!</div>
+        );
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: __dirname + '/build',
+        publicPath: '/',
+        filename: 'bundle.js' },
+    devServer: { contentBase: './build' },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'eslint-loader']
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({ template: path.resolve('./index.html') })]
+};
+// };
